4ec490a25ce63585c0e5a25d2d7373f75ce4e85a
Kévin LORDINOT <kev97250@hotmail.fr>
2024-01-18 23:17:17 +0100

    add comment

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class HomeController extends AbstractController
 {
+    /**
+     * Display Home page
+     */
     #[Route('/', name: 'app_home')]
-    public function index(SessionInterface $session): Response
+    public function index(): Response
     {
-       
-        
         return $this->render('home/index.html.twig');
     }
 }
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,33 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-// use Symfony\Component\Security\Core\Security;
+
+
 
 class LoginController extends AbstractController
 {
-    // private $security;
-
-    // public function __construct(Security $security){
-    //     $this->security=$security;
-    // }
+    /**
+     * login's form
+     */
   
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils, SessionInterface $session): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
          $error = $authenticationUtils->getLastAuthenticationError();
           // last username entered by the user
          $lastUsername = $authenticationUtils->getLastUsername();
 
-        //  getUser() methode qui vient de l'abstractController, retourne un objet
-        
-         // last username entered by the user
+        //  getUser() methode qui vient de l'abstractController, retourne un objet User si
+        // il y a UserInterface qui est implémenter dans Entity\User
          return $this->render('login/index.html.twig', [
                      'last_username' => $lastUsername,
                     'error'         => $error,
              ]);
     }
+    /**
+     * logout
+     */
     #[Route('/logout', name: 'app_logout')]
     public function logout(){
         
